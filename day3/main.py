841da09360546fbc824bc09e06348cd20f767e6b
kwjay <kwjay1002@gmail.com>
2023-12-05 23:25:26 +0100

    to-be-changed string manipulation

--- day3/main.py
+++ day3/main.py
@@ -1,2 +1,18 @@
+
+
+def check_for_digit(positions, line:str):
+    for position in positions:
+        if position != 0 and position != len(line):
+            if line[position].isdigit():
+                return line[position]
+
+
+
 with open("input.txt") as data_file:
     data = data_file.readlines()
+for line_index in range(0, len(data)):
+    text_line = data[line_index]
+    for char_index in range(0, len(text_line)):
+        if not text_line[char_index].isdigit() and text_line[char_index] != ".":
+            places_to_check = [char_index-1, char_index, char_index+1]
+
